117eb8a49a962da9262f0e558cd8b08f9ab78b6f
igotbitches <check.point9917@yandex.ru>
2021-01-25 21:48:08 +0300

    feat: create functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined || array.length === 0){
+        return 0;
+    }
+
+    let minNum = array[0];
+    for (let i = 0; i < array.length; i++)
+        if (minNum > array[i])
+            minNum = array[i];
+    return minNum;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length === 0){
+        return 0;
+    }
+
+    let maxNum = array[0];
+    for (let i = 0; i < array.length; i++)
+        if (maxNum < array[i])
+            maxNum = array[i];
+    return maxNum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length === 0){
+        return 0;
+    }
+
+    let sum = 0
+    for (let i = 0; i < array.length; i++)
+        sum += array[i]
+    return sum / array.length;
 }
